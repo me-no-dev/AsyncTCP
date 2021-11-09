5a4d11269cb4acf653254ff65a66c378a2baee41
Bruno Joyal <brunojoyal@gmail.com>
2021-11-09 16:20:28 -0500

    config async stack size

--- src/AsyncTCP.cpp
+++ src/AsyncTCP.cpp
@@ -31,6 +31,7 @@ extern "C"{
 }
 #include "esp_task_wdt.h"
 
+#define CONFIG_ASYNC_TCP_STACK 2*8192
 /*
  * TCP/IP Event Task
  * */
@@ -218,7 +219,7 @@ static bool _start_async_task(){
         return false;
     }
     if(!_async_service_task_handle){
-        xTaskCreateUniversal(_async_service_task, "async_tcp", 8192*2, NULL, 3, &_async_service_task_handle, CONFIG_ASYNC_TCP_RUNNING_CORE);
+        xTaskCreateUniversal(_async_service_task, "async_tcp", CONFIG_ASYNC_TCP_STACK, NULL, 3, &_async_service_task_handle, CONFIG_ASYNC_TCP_RUNNING_CORE);
         if(!_async_service_task_handle){
             return false;
         }
