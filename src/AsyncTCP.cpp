74420db09b01872cccaeef91970906cf476a0a47
Henrik Östman <trycoon@gmail.com>
2022-07-05 12:12:36 +0200

    Applied #144
    https://github.com/me-no-dev/AsyncTCP/pull/144/files

--- src/AsyncTCP.cpp
+++ src/AsyncTCP.cpp
@@ -1017,8 +1017,11 @@ size_t AsyncClient::write(const char* data) {
 
 size_t AsyncClient::write(const char* data, size_t size, uint8_t apiflags) {
     size_t will_send = add(data, size, apiflags);
-    if(!will_send || !send()) {
+    if (!will_send) {
         return 0;
+    }
+    while (connected() && !send()) {
+        taskYIELD();
     }
     return will_send;
 }
