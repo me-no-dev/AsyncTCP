b2f4477d47e00a3c1efa797856287f480a8bd9d1
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-12-15 18:17:42 +0100

    v3.3.0

--- README.md
+++ README.md
@@ -43,7 +43,7 @@ The base classes on which everything else is built. They expose all possible sce
 ## Coordinates
 
 ```
-mathieucarbou/AsyncTCP @ ^3.2.15
+mathieucarbou/AsyncTCP @ ^3.3.0
 ```
 
 ## Important recommendations
--- src/AsyncTCP.h
+++ src/AsyncTCP.h
@@ -22,10 +22,10 @@
 #ifndef ASYNCTCP_H_
 #define ASYNCTCP_H_
 
-#define ASYNCTCP_VERSION          "3.2.15"
+#define ASYNCTCP_VERSION          "3.3.0"
 #define ASYNCTCP_VERSION_MAJOR    3
-#define ASYNCTCP_VERSION_MINOR    2
-#define ASYNCTCP_VERSION_REVISION 15
+#define ASYNCTCP_VERSION_MINOR    3
+#define ASYNCTCP_VERSION_REVISION 0
 #define ASYNCTCP_FORK_mathieucarbou
 
 #include "IPAddress.h"
